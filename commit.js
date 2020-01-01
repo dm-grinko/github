a65d04f852cc6361364f5f434912c046cb97d59e
Dmitry Grinko <dm.grinko@gmail.com>
2020-01-01 12:12:45 -0800

    commit 2020-1-01 12:12:45

--- commit.js
+++ commit.js
@@ -12,20 +12,24 @@ const years = [
     2012,
 ];
 
-const commit = async(YYYY) => {
-    // for each month
-    for (let MM = 3; MM <= 12; MM++) {
-        // get all days
-        const days = moment(`${YYYY}-${MM}`, "YYYY-MM").daysInMonth();
-        //  for each day
-        for (let day = 1; day <= days; day++) {
-            const DD = ("0" + day).slice(-2);
-            // generate date
-            const date = `${YYYY}-${MM}-${DD} 12:12:45`;
-            // git commit
-            await shell.exec(`./commit.sh "${date}"`);
+const commit = async(years) => {
+    const promises = [];
+    // for each year
+    for (let y = 0; y < years.length; y++) {
+        const YYYY = years[y];
+        // for each month
+        for (let MM = 1; MM <= 2; MM++) {
+            // get all days
+            const days = moment(`${YYYY}-${MM}`, "YYYY-MM").daysInMonth();
+            //  for each day
+            for (let day = 1; day <= days; day++) {
+                const DD = ("0" + day).slice(-2);
+                // git commit
+                promises.push(shell.exec(`./commit.sh "${YYYY}-${MM}-${DD} 12:12:45"`));
+            }
         }
     }
+    await Promise.all(promises);
 };
 
-years.forEach(async(YYYY) => commit(YYYY));
+commit(years);
